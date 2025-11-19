a8158539c3bc2e68d99d668a2fc84dde03e76a78
ta3wook <taewook02@mju.ac.kr>
2025-11-19 15:47:41 +0900

    feat: (IaC) dev, prod 환경 분리

--- iac/base/variables.tf
+++ iac/base/variables.tf
@@ -6,8 +6,13 @@ variable "aws_region" {
   default     = "ap-northeast-2" # 서울 리전
 }
 
+variable "environment" {
+  type = string
+}
+
 variable "qwik_frontend_bucket_name" {
   description = "FE bucket name"
   type = string
   default = "qwik-frontend-bucket-2025"
 }
+
